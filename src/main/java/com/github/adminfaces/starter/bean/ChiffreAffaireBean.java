c4fadf64591f6cbf01c1da783e312f2aeca2d8cd
calviniloki <calviniloki@localhost>
2019-10-18 13:12:39 +0100

    Signed-off-by:calviniloki <calviniloki@localhost>

--- src/main/java/com/github/adminfaces/starter/bean/ChiffreAffaireBean.java
+++ src/main/java/com/github/adminfaces/starter/bean/ChiffreAffaireBean.java
@@ -1,6 +1,7 @@
 package com.github.adminfaces.starter.bean;
 
 
+import com.github.adminfaces.starter.model.ChiffrePrestation;
 import java.io.Serializable;
 import java.util.List;
 
@@ -12,6 +13,9 @@ import org.omnifaces.cdi.ViewScoped;
 import com.github.adminfaces.starter.service.ChiffreAffaireService;
 import com.github.adminfaces.starter.service.PrestService;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 
 /**
@@ -25,38 +29,77 @@ public class ChiffreAffaireBean implements Serializable {
     private List<Integer> years;
     private Integer anneeDebut;
     private Integer anneeFin;
-    private List<ChiffreAffaireService> list;
+    private Map<String,String> listPrest;
+    private List<String> keyList;
+    
+    @Inject
+    private PrestService prestService;
         
     @PostConstruct
     public void init(){
-        this.yearCount = 5;
+        anneeDebut = 2015;
+        anneeFin = 2019;
+        yearCount = 5;
         years =new ArrayList<>(yearCount);
         years.add(2015);
         years.add(2016);
         years.add(2017);
         years.add(2018);
         years.add(2019);
-        list = new ArrayList<>();
-        list.add(new ChiffreAffaireService("1", "REDEVANCE MARCHANDISE"));
-        list.add(new ChiffreAffaireService("5", "REDEVANCE NAVIRE"));
-        list.add(new ChiffreAffaireService("7", "REDEVANCE DOMANIALE"));
-        list.add(new ChiffreAffaireService("2", "REDEVANCE CONCESSION"));
-        list.add(new ChiffreAffaireService("4", "REDEVANCE ELECTRICITE"));
-        list.add(new ChiffreAffaireService("6", "REDEVANCE DIVERS"));
-        list.add(new ChiffreAffaireService("A", "REDEVANCE A"));
         
+        listPrest = new HashMap<>();
+        listPrest.put("1", "REDEVANCE MARCHANDISE");
+        listPrest.put("5", "REDEVANCE NAVIRE");
+        listPrest.put("7", "REDEVANCE DOMANIALE");
+        listPrest.put("2", "REDEVANCE CONCESSION");
+        listPrest.put("4", "REDEVANCE ELECTRICITE");
+        listPrest.put("6", "REDEVANCE DIVERS");
+        listPrest.put("A", "REDEVANCE A");
+        keyList = new ArrayList<>(listPrest.keySet().stream().sorted().collect(Collectors.toList()));
     }
     
     public Double totalAnnee(Integer annee){
         Double total = 0.0 ;
-        for(ChiffreAffaireService ca : list){
-            total += ca.getChiffre(annee);
+        for(String code : listPrest.keySet()){
+            total += getMontantPrest(annee, code);
         }
         return total;
     }
     
-    public void findByYear(){
-        System.out.println("De " + anneeDebut + " à " + anneeFin);
+    public Double getMontantPrest(Integer annee, String code){
+        List<ChiffrePrestation> list = prestService.getList().stream()
+                .filter(ca -> (ca.getName().equalsIgnoreCase(code)) && (ca.getAnnee().intValue() == annee.intValue()))
+                .collect(Collectors.toList());
+        
+            ChiffrePrestation cp = list.get(0);
+            return cp.getMontant();
+    }
+    
+    public void findByYear(Integer debut, Integer fin){
+        prestService.listByAn(debut, fin);
+        anneeDebut = debut;
+        anneeFin = fin;
+        yearCount = fin - debut + 1;
+        years.clear();
+        for(int i=debut;i <fin+1;i++){
+            years.add(i);
+        }
+    }
+    
+    public String getPrestLibelle(String code){
+        String libelle = null;
+        switch(code){
+                    case "1": libelle = "REDEVANCE MARCHANDISE";break;
+                    case "2": libelle = "REDEVANCE CONCESSION";break;
+                    case "4": libelle = "REDEVANCE ELECTRICITE";break;
+                    case "5": libelle = "REDEVANCE NAVIRE";break;
+                    case "6": libelle = "REDEVANCE DIVERS";break;
+                    case "7": libelle = "REDEVANCE DOMANIALE";break;
+                    case "A": libelle = "AUTRES REDEVANCES";break;
+                    default: ;                           
+                }
+        System.out.println("Libelle " + libelle);
+        return libelle;
     }
     public Integer getYearCount() {
         return yearCount;
@@ -74,13 +117,22 @@ public class ChiffreAffaireBean implements Serializable {
         this.years = years;
     }
 
-    public List<ChiffreAffaireService> getList() {
-        return list;
+    public Map<String, String> getListPrest() {
+        return listPrest;
     }
 
-    public void setList(List<ChiffreAffaireService> list) {
-        this.list = list;
+    public void setListPrest(Map<String, String> listPrest) {
+        this.listPrest = listPrest;
     }
+
+    public List<String> getKeyList() {
+        return keyList;
+    }
+
+    public void setKeyList(List<String> keyList) {
+        this.keyList = keyList;
+    }
+
 
     public Integer getAnneeDebut() {
         return anneeDebut;
--- src/main/java/com/github/adminfaces/starter/service/PrestService.java
+++ src/main/java/com/github/adminfaces/starter/service/PrestService.java
@@ -12,6 +12,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
@@ -28,119 +30,126 @@ import org.apache.deltaspike.data.impl.criteria.QueryCriteria;
  *
  * @author Calvin ILOKI
  */
+@Singleton
+@Startup
+public class PrestService implements Serializable {
+//
+//    private HashMap<Integer, Double> redevNavire;
+//    private HashMap<Integer, Double> redevDivers;
+//    private HashMap<Integer, Double> redevDom;
+//    private HashMap<Integer, Double> redevMarch;
+//    private HashMap<Integer, Double> redevConcession;
+//    private HashMap<Integer, Double> redevElect;
+//    private HashMap<Integer, Double> redevAutre;
 
-@Stateless
-public class PrestService implements Serializable{
-
-    private HashMap<Integer, Double> redevNavire;
-    private HashMap<Integer, Double> redevDivers;
-    private HashMap<Integer, Double> redevDom;
-    private HashMap<Integer, Double> redevMarch;
-    private HashMap<Integer, Double> redevConcession;
-    private HashMap<Integer, Double> redevElect;
-    private HashMap<Integer, Double> redevAutre;
-
-    public PrestService() {
-        
-    }
-    
+    private List<ChiffrePrestation> list;
     @Inject
     private EntityManager entityManager;
-    
+
     public <E extends PersistenceEntity> Criteria<E, E> criteria(Class<E> entityClass) {
         return new QueryCriteria<>(entityClass, entityClass, getEntityManager());
     }
-    public List<ChiffrePrestation> listByAn(Integer annee) {
-        return criteria(ChiffrePrestation.class)
-                .eq(ChiffrePrestation_.annee, annee)
-                .orderDesc(ChiffrePrestation_.montant)
+
+    public void listByAn(Integer debut, Integer fin) {
+        Logger.getGlobal().log(Level.INFO, "Recherche du chiffre d''affaire par type de prestation entre {0} et {1}.", new Object[]{debut.toString(), fin.toString()});
+        list = criteria(ChiffrePrestation.class)
+                .between(ChiffrePrestation_.annee, debut, fin)
                 .getResultList();
     }
-    
+
+    public ChiffrePrestation getPrestParAn(String code, Integer annee) {
+        return criteria(ChiffrePrestation.class)
+                .eq(ChiffrePrestation_.annee, annee)
+                .eqIgnoreCase(ChiffrePrestation_.name, code)
+                .orderDesc(ChiffrePrestation_.montant)
+                .getSingleResult();
+    }
+
     @PostConstruct
-    public void init(){
-        redevAutre = new HashMap<>();
-        redevConcession = new HashMap<>();
-        redevDivers = new HashMap<>();
-        redevDom = new HashMap<>();
-        redevElect = new HashMap<>();
-        redevMarch = new HashMap<>();
-        redevNavire = new HashMap<>();
-        
-        for(int i = 2015 ; i < 2020; i++){
-            List<ChiffrePrestation> list = listByAn(i);
-            list.forEach(prest -> {
-//                System.out.println(prest.getAnnee() + " - " + prest.getName() + " : " + prest.getMontant());
-                switch(prest.getName()){
-                    case "1": redevMarch.put(prest.getAnnee(), prest.getMontant());break;
-                    case "2": redevConcession.put(prest.getAnnee(), prest.getMontant());break;
-                    case "4": redevElect.put(prest.getAnnee(), prest.getMontant());break;
-                    case "5": redevNavire.put(prest.getAnnee(), prest.getMontant());break;
-                    case "6": redevDivers.put(prest.getAnnee(), prest.getMontant());break;
-                    case "7": redevDom.put(prest.getAnnee(), prest.getMontant());break;
-                    case "A": redevAutre.put(prest.getAnnee(), prest.getMontant());break;
-                    default: System.out.println("RIEN");                           
-                }
-            });
-        }
+    public void init() {
+        Logger.getGlobal().info(" PrestService est initialisé...");
+        listByAn(2015, 2019);
+//        redevAutre = new HashMap<>();
+//        redevConcession = new HashMap<>();
+//        redevDivers = new HashMap<>();
+//        redevDom = new HashMap<>();
+//        redevElect = new HashMap<>();
+//        redevMarch = new HashMap<>();
+//        redevNavire = new HashMap<>();
+//        
+//        for(int i = 2015 ; i < 2020; i++){
+//            List<ChiffrePrestation> list = listByAn(i);
+//            list.forEach(prest -> {
+////                System.out.println(prest.getAnnee() + " - " + prest.getName() + " : " + prest.getMontant());
+//                switch(prest.getName()){
+//                    case "1": redevMarch.put(prest.getAnnee(), prest.getMontant());break;
+//                    case "2": redevConcession.put(prest.getAnnee(), prest.getMontant());break;
+//                    case "4": redevElect.put(prest.getAnnee(), prest.getMontant());break;
+//                    case "5": redevNavire.put(prest.getAnnee(), prest.getMontant());break;
+//                    case "6": redevDivers.put(prest.getAnnee(), prest.getMontant());break;
+//                    case "7": redevDom.put(prest.getAnnee(), prest.getMontant());break;
+//                    case "A": redevAutre.put(prest.getAnnee(), prest.getMontant());break;
+//                    default: System.out.println("RIEN");                           
+//                }
+//            });
+//        }
     }
 
-    public HashMap<Integer, Double> getRedevNavire() {
-        return redevNavire;
-    }
-
-    public void setRedevNavire(HashMap<Integer, Double> redevNavire) {
-        this.redevNavire = redevNavire;
-    }
-
-    public HashMap<Integer, Double> getRedevDivers() {
-        return redevDivers;
-    }
-
-    public void setRedevDivers(HashMap<Integer, Double> redevDivers) {
-        this.redevDivers = redevDivers;
-    }
-
-    public HashMap<Integer, Double> getRedevDom() {
-        return redevDom;
-    }
-
-    public void setRedevDom(HashMap<Integer, Double> redevDom) {
-        this.redevDom = redevDom;
-    }
-
-    public HashMap<Integer, Double> getRedevMarch() {
-        return redevMarch;
-    }
-
-    public void setRedevMarch(HashMap<Integer, Double> redevMarch) {
-        this.redevMarch = redevMarch;
-    }
-
-    public HashMap<Integer, Double> getRedevConcession() {
-        return redevConcession;
-    }
-
-    public void setRedevConcession(HashMap<Integer, Double> redevConcession) {
-        this.redevConcession = redevConcession;
-    }
-
-    public HashMap<Integer, Double> getRedevElect() {
-        return redevElect;
-    }
-
-    public void setRedevElect(HashMap<Integer, Double> redevElect) {
-        this.redevElect = redevElect;
-    }
-
-    public HashMap<Integer, Double> getRedevAutre() {
-        return redevAutre;
-    }
-
-    public void setRedevAutre(HashMap<Integer, Double> redevAutre) {
-        this.redevAutre = redevAutre;
-    }
-
+//    public HashMap<Integer, Double> getRedevNavire() {
+//        return redevNavire;
+//    }
+//
+//    public void setRedevNavire(HashMap<Integer, Double> redevNavire) {
+//        this.redevNavire = redevNavire;
+//    }
+//
+//    public HashMap<Integer, Double> getRedevDivers() {
+//        return redevDivers;
+//    }
+//
+//    public void setRedevDivers(HashMap<Integer, Double> redevDivers) {
+//        this.redevDivers = redevDivers;
+//    }
+//
+//    public HashMap<Integer, Double> getRedevDom() {
+//        return redevDom;
+//    }
+//
+//    public void setRedevDom(HashMap<Integer, Double> redevDom) {
+//        this.redevDom = redevDom;
+//    }
+//
+//    public HashMap<Integer, Double> getRedevMarch() {
+//        return redevMarch;
+//    }
+//
+//    public void setRedevMarch(HashMap<Integer, Double> redevMarch) {
+//        this.redevMarch = redevMarch;
+//    }
+//
+//    public HashMap<Integer, Double> getRedevConcession() {
+//        return redevConcession;
+//    }
+//
+//    public void setRedevConcession(HashMap<Integer, Double> redevConcession) {
+//        this.redevConcession = redevConcession;
+//    }
+//
+//    public HashMap<Integer, Double> getRedevElect() {
+//        return redevElect;
+//    }
+//
+//    public void setRedevElect(HashMap<Integer, Double> redevElect) {
+//        this.redevElect = redevElect;
+//    }
+//
+//    public HashMap<Integer, Double> getRedevAutre() {
+//        return redevAutre;
+//    }
+//
+//    public void setRedevAutre(HashMap<Integer, Double> redevAutre) {
+//        this.redevAutre = redevAutre;
+//    }
     public EntityManager getEntityManager() {
         return entityManager;
     }
@@ -148,7 +157,13 @@ public class PrestService implements Serializable{
     public void setEntityManager(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
-    
-    
+
+    public List<ChiffrePrestation> getList() {
+        return list;
+    }
+
+    public void setList(List<ChiffrePrestation> list) {
+        this.list = list;
+    }
 
 }
