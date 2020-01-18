f6023cc5e8507a20be2716ada816f7c2cbad324a
Calvin ILOKI <Calvin ILOKI@DESKTOP-23I5HNI.papncg.local>
2020-01-18 11:12:19 +0000

    Merge origin/master
    Conflicts:
    	build.xml
    	manifest.mf
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/main/java/com/github/adminfaces/starter/repos/TdrCongoTerminalRepository.java
+++ src/main/java/com/github/adminfaces/starter/repos/TdrCongoTerminalRepository.java
@@ -13,31 +13,31 @@ public interface TdrCongoTerminalRepository extends EntityRepository<TdrCongoTer
     @Query("SELECT sum(t.evp) FROM TdrCongoTerminal t WHERE t.numero is not null and t.annee = :annee")
     BigInteger getTotalEVPParAn(@QueryParam("annee") String annee);
     
-    @Query("SELECT sum(t.evp) FROM TdrCongoTerminal t WHERE t.trafic = 'IMP' and t.annee = :annee")
+    @Query("SELECT sum(t.evp) FROM TdrCongoTerminal t WHERE t.numero is not null and t.trafic = 'IMP' and t.annee = :annee")
     BigInteger getTotalImportEVPParAn(@QueryParam("annee") String annee);
     
-    @Query("SELECT sum(t.evp) FROM TdrCongoTerminal t WHERE t.trafic = 'EXP' and t.annee = :annee")
+    @Query("SELECT sum(t.evp) FROM TdrCongoTerminal t WHERE t.numero is not null and t.trafic = 'EXP' and t.annee = :annee")
     BigInteger getTotalExportEVPParAn(@QueryParam("annee") String annee);
     
-    @Query("SELECT sum(t.evp) FROM TdrCongoTerminal t WHERE t.trafic = 'IMP1' and t.annee = :annee")
+    @Query("SELECT sum(t.evp) FROM TdrCongoTerminal t WHERE t.numero is not null and t.trafic = 'IMP1' and t.annee = :annee")
     BigInteger getTotalTransitEVPParAn(@QueryParam("annee") String annee);
     
     @Query("SELECT sum(t.evp) FROM TdrCongoTerminal t WHERE t.trafic like 'TRB%' and t.annee = :annee")
     BigInteger getTotalTransboEVPParAn(@QueryParam("annee") String annee);
     
-    @Query("SELECT sum(t.poids) FROM TdrCongoTerminal t WHERE t.annee = :annee")
+    @Query("SELECT sum(t.poids) FROM TdrCongoTerminal t WHERE t.numero is not null and t.annee = :annee")
     BigInteger getTotalPoidsConteneurParAn(@QueryParam("annee") String annee);
     
-    @Query("SELECT sum(t.poids) FROM TdrCongoTerminal t WHERE t.trafic = 'IMP' and t.annee = :annee")
+    @Query("SELECT sum(t.poids) FROM TdrCongoTerminal t WHERE t.numero is not null and t.trafic = 'IMP' and t.annee = :annee")
     BigInteger getTotalImportTonnageParAn(@QueryParam("annee") String annee);
     
-    @Query("SELECT sum(t.poids) FROM TdrCongoTerminal t WHERE t.trafic = 'EXP' and t.annee = :annee")
+    @Query("SELECT sum(t.poids) FROM TdrCongoTerminal t WHERE t.numero is not null and t.trafic = 'EXP' and t.annee = :annee")
     BigInteger getTotalExportTonnageParAn(@QueryParam("annee") String annee);
     
-    @Query("SELECT sum(t.poids) FROM TdrCongoTerminal t WHERE t.trafic = 'IMP1' and t.annee = :annee")
+    @Query("SELECT sum(t.poids) FROM TdrCongoTerminal t WHERE t.numero is not null and t.trafic = 'IMP1' and t.annee = :annee")
     BigInteger getTotalTransitTonnageParAn(@QueryParam("annee") String annee);
     
-    @Query("SELECT sum(t.poids) FROM TdrCongoTerminal t WHERE t.trafic like 'TRB%' and t.annee = :annee")
+    @Query("SELECT sum(t.poids) FROM TdrCongoTerminal t WHERE t.numero is not null and t.trafic like 'TRB%' and t.annee = :annee")
     BigInteger getTotalTransboTonnageParAn(@QueryParam("annee") String annee);
     
 }
