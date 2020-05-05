b3862f26217b4723ea73e292d49e32b9542f701c
Calvin ILOKI <Calvin ILOKI@DESKTOP-23I5HNI.papncg.local>
2020-05-05 15:39:46 +0200

    Merge origin/master
    Conflicts:
    	payara/config/domain.xml
    	src/main/webapp/escale.xhtml

--- src/main/java/com/github/adminfaces/starter/bean/InitAppBean.java
+++ src/main/java/com/github/adminfaces/starter/bean/InitAppBean.java
@@ -27,7 +27,12 @@ public class InitAppBean implements Serializable {
 
     @PostConstruct
     public void init() {
+        adminConfig.setRenderFormAsterisks(true);
         adminConfig.setDateFormat("dd/MM/yyyy HH:mm:ss");
-        
+//        IntStream.rangeClosed(1, 50).forEach(i -> create(i));
+    }
+    
+    private void create(int i) {
+        crudService.insert(new Car().model("model " + i).name("name" + i).price(Double.valueOf(i)));
     }
 }
