799c49efafffd9f864d3a33249a5e91d339b709d
Calvin ILOKI <Calvin ILOKI@DESKTOP-23I5HNI.papncg.local>
2020-05-07 11:51:04 +0200

    Merge origin/master
    Conflicts:
    	payara/config/domain.xml
    	src/main/webapp/escale.xhtml

--- src/main/java/com/github/adminfaces/starter/bean/ImportExcel.java
+++ src/main/java/com/github/adminfaces/starter/bean/ImportExcel.java
@@ -82,37 +82,42 @@ public class ImportExcel {
                     continue;
                 }
                 stmt.setInt(1, Integer.valueOf(excelFile.getName().substring(0, 6))); //MOIS                
-                stmt.setString(2, row.getCell(0).toString()); //NUM_CTN
-                stmt.setString(3, row.getCell(11) == null ? "" :row.getCell(11).toString()); //DAT
-                stmt.setString(4, row.getCell(1) == null ? "" :row.getCell(1).toString()); //MOUVEMENT
-                stmt.setString(5, row.getCell(10) == null ? "" :row.getCell(10).toString()); //TRAFIC
-                stmt.setString(6, row.getCell(9) == null ? "" :row.getCell(9).toString()); //VIDE PLEIN
-                stmt.setString(7, row.getCell(3) == null ? "" :row.getCell(3).toString());//ISO
-                stmt.setString(8, row.getCell(4) == null ? "" :row.getCell(4).toString());//TARE
-                stmt.setString(9, row.getCell(7) == null ? "" :row.getCell(7).toString());//EXP COURS
-                stmt.setString(10, row.getCell(14) == null ? "" :row.getCell(14).toString());//NAVIRE
-                stmt.setString(11, row.getCell(15) == null ? "" : row.getCell(15).toString()); //VOYAGE
-                stmt.setString(12, row.getCell(21) == null ? "" : row.getCell(21).toString());//POL
-                stmt.setString(13, row.getCell(22) == null ? "" : row.getCell(22).toString());//POD
-                stmt.setString(14, row.getCell(8) == null ? "" :row.getCell(8).toString());//ARMATEUR
-                stmt.setString(15, row.getCell(2) == null ? "" :row.getCell(2).toString());//POIDS
-                stmt.setString(16, row.getCell(28) == null ? "" :row.getCell(28).toString());//DATE IN
-                stmt.setString(17, row.getCell(29) == null ? "" :row.getCell(29).toString());//DATE MOVE
-                stmt.setString(18, row.getCell(4) == null ? "" :row.getCell(4).toString()); //TYPE
-                stmt.setString(19, row.getCell(5) == null ? "" :row.getCell(5).toString()); //EVP
-                stmt.setString(20, row.getCell(12) == null ? "" :row.getCell(12).toString()); //PARC
-                stmt.setString(21, row.getCell(13) == null ? "" :row.getCell(13).toString());//TRUCK VESSEL
-                stmt.setString(22, row.getCell(16) == null ? "" :row.getCell(16).toString());//TYPE NAVIRE
-                stmt.setString(23, row.getCell(17) == null ? "" :row.getCell(17).toString());//DGX
-                stmt.setString(24, row.getCell(18) == null ? "" :row.getCell(18).toString());//REFF
-                stmt.setString(25, row.getCell(19) == null ? "" :row.getCell(19).toString()); //OOG
-                stmt.setString(26, row.getCell(20) == null ? "" :row.getCell(20).toString()); //OPL
-                stmt.setString(27, row.getCell(23) == null ? "" :row.getCell(23).toString()); //PDESF
-                stmt.setString(28, row.getCell(24) == null ? "" :row.getCell(24).toString());//PRESENCE
-                stmt.setString(29, row.getCell(25) == null ? "" :row.getCell(25).toString());//TRUCK VESSEL IN
-                stmt.setString(30, row.getCell(26) == null ? "" :row.getCell(26).toString());//NAVIRE IN
-                stmt.setString(31, row.getCell(15) == null ? "" :row.getCell(15).toString());//VOYAGE IN
-                stmt.setString(32, row.getCell(16) == null ? "" :row.getCell(16).toString());//TYPE IN
+                    stmt.setString(2, row.getCell(0).toString()); //NUM_CTN
+                    stmt.setString(3, row.getCell(11) == null ? "" : row.getCell(11).toString()); //DAT
+                    stmt.setString(4, row.getCell(1) == null ? ""
+                            : row.getCell(1).toString().trim().equalsIgnoreCase("DSCH") ? "DEBA"
+                            : row.getCell(1).toString().trim().equalsIgnoreCase("LOAD") ? "EMBA" : ""); //MVNT
+                    stmt.setString(5, row.getCell(10) == null ? ""
+                            : row.getCell(10).toString().trim().equalsIgnoreCase("Transbo") ? "T"
+                            : row.getCell(10).toString().trim().equalsIgnoreCase("Import") ? "I"
+                            : row.getCell(10).toString().trim().equalsIgnoreCase("Export") ? "E" : ""); //TRAFIC
+                    stmt.setString(6, row.getCell(9) == null ? "" : row.getCell(9).toString()); //P V
+                    stmt.setString(7, row.getCell(2) == null ? "" : row.getCell(2).toString());//ISO
+                    stmt.setString(8, row.getCell(3) == null ? "" : row.getCell(3).toString());//TARE
+                    stmt.setString(9, row.getCell(6) == null ? "" : row.getCell(6).toString());//EXPLOITANT
+                    stmt.setString(10, row.getCell(14) == null ? "" : row.getCell(14).toString());//NAVIRE
+                    stmt.setString(11, row.getCell(15) == null ? "" : row.getCell(15).toString()); //VOYAGE
+                    stmt.setString(12, row.getCell(23) == null ? "" : row.getCell(23).toString());//POL
+                    stmt.setString(13, row.getCell(24) == null ? "" : row.getCell(24).toString());//POD
+                    stmt.setString(14, row.getCell(8) == null ? "" : row.getCell(8).toString());//OWNER
+                    stmt.setString(15, row.getCell(5) == null ? "" : row.getCell(5).toString().replace(".0", ""));//POIDS
+                    stmt.setString(16, row.getCell(17) == null ? "" : row.getCell(17).toString());//DATE ATA
+                    stmt.setString(17, row.getCell(18) == null ? "" : row.getCell(18).toString());//DATE ATD
+                    stmt.setString(18, row.getCell(3) == null ? "" : row.getCell(3).toString()); //TYPE
+                    stmt.setString(19, row.getCell(4) == null ? "" : row.getCell(4).toString().replace(".0", "")); //EVP
+                    stmt.setString(20, ""); //PARC
+                    stmt.setString(21, row.getCell(12) == null ? "" : row.getCell(12).toString());//TRUCK VESSEL
+                    stmt.setString(22, row.getCell(16) == null ? "" : row.getCell(16).toString());//TYPE NAVIRE
+                    stmt.setString(23, row.getCell(19) == null ? "" : row.getCell(19).toString());//DGX
+                    stmt.setString(24, row.getCell(20) == null ? "" : row.getCell(20).toString());//REFF
+                    stmt.setString(25, row.getCell(21) == null ? "" : row.getCell(21).toString()); //OOG
+                    stmt.setString(26, row.getCell(22) == null ? "" : row.getCell(22).toString()); //OPL
+                    stmt.setString(27, row.getCell(25) == null ? "" : row.getCell(25).toString()); //PDESF
+                    stmt.setString(28, row.getCell(26) == null ? "" : row.getCell(26).toString().replace(".0", ""));//PRESENCE
+                    stmt.setString(29, "");//TRUCK VESSEL IN
+                    stmt.setString(30, "");//NAVIRE IN
+                    stmt.setString(31, "");//VOYAGE IN
+                    stmt.setString(32, "");//TYPE IN 
                     if (i%1000 == 0) {
                         System.out.print("=");
                     }
